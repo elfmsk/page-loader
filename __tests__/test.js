a1cb7bac33d8ff25ff3120d756b1d11b52afae98
elfmsk <elfmsk@mail.ru>
2019-03-13 12:20:54 +0300

    step 2.3

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,44 +4,66 @@ import os from 'os';
 import { promises as fs } from 'fs';
 import downloadFiles from '../src';
 
-const fsExtra = require('fs-extra');
-
-const pathToExpectedFile = path.resolve(__dirname, '__fixtures__/sitesedona-github-io.html');
+const pathToBaseFile = path.resolve(__dirname, '__fixtures__/sitesedona-github-io.html');
+const pathToLogoPng = path.resolve(__dirname, '__fixtures__/sitesedona-github-io_files/logo.png');
+const pathToMainCss = path.resolve(__dirname, '__fixtures__/sitesedona-github-io_files/main.css');
+const pathToMapPng = path.resolve(__dirname, '__fixtures__/sitesedona-github-io_files/map.png');
+const pathToNormalize = path.resolve(__dirname, '__fixtures__/sitesedona-github-io_files/normalize.css');
+const pathToWelcomePng = path.resolve(__dirname, '__fixtures__/sitesedona-github-io_files/welcome.png');
 
 let timeDir;
 beforeAll(async () => {
   timeDir = await fs.mkdtemp(path.join(__dirname, os.tmpdir()));
-});
 
-// test('downloadFile', async () => {
-//   nock('https://sitesedona.github.io')
-//     .get('/')
-//     .replyWithFile(200, pathToExpectedFile);
-//
-//   await downloadFiles('https://sitesedona.github.io', timeDir);
-//
-//   const expected = await fs.readFile(pathToExpectedFile, 'utf8');
-//   const result = await fs.readFile(`${timeDir}/sitesedona-github-io.html`, 'utf8');
-//
-//   expect(expected).toEqual(result);
-// });
-//
-// afterAll(async () => {
-//   await fsExtra.remove(timeDir);
-// });
-
-test('downloadFile', async () => {
-  // nock('https://sitesedona.github.io')
-  //   .get('/courses')
-  //   .replyWithFile(200, pathToExpectedFile);
+  nock('https://sitesedona.github.io')
+    .get('/')
+    .replyWithFile(200, pathToBaseFile);
+  nock('https://sitesedona.github.io')
+    .get('/img/logo.png')
+    .replyWithFile(200, pathToLogoPng);
+  nock('https://sitesedona.github.io')
+    .get('/css/main.css')
+    .replyWithFile(200, pathToMainCss);
+  nock('https://sitesedona.github.io')
+    .get('/img/map.png')
+    .replyWithFile(200, pathToMapPng);
+  nock('https://sitesedona.github.io')
+    .get('/css/normalize.css')
+    .replyWithFile(200, pathToNormalize);
+  nock('https://sitesedona.github.io')
+    .get('/img/welcome.png')
+    .replyWithFile(200, pathToWelcomePng);
 
   await downloadFiles('https://sitesedona.github.io', timeDir);
-  // const expected = await fs.readFile(pathToExpectedFile, 'utf8');
-  // const result = await fs.readFile(`${timeDir}/hexlet-io-courses.html`, 'utf8');
-
-  // expect(expected).toEqual(result);
 });
 
-afterAll(async () => {
-  await setTimeout(fsExtra.remove, 20000, timeDir);
+it('downloadBase', async () => {
+  const expected = await fs.readFile(pathToBaseFile, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io.html`, 'utf8');
+  expect(expected).toEqual(result);
+});
+it('downloadLogo', async () => {
+  const expected = await fs.readFile(pathToLogoPng, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io_files/logo.png`, 'utf8');
+  expect(expected).toEqual(result);
+});
+it('downloadMainCss', async () => {
+  const expected = await fs.readFile(pathToMainCss, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io_files/main.css`, 'utf8');
+  expect(expected).toEqual(result);
+});
+it('downloadMapPng', async () => {
+  const expected = await fs.readFile(pathToMapPng, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io_files/map.png`, 'utf8');
+  expect(expected).toEqual(result);
+});
+it('downloadNormalize', async () => {
+  const expected = await fs.readFile(pathToNormalize, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io_files/normalize.css`, 'utf8');
+  expect(expected).toEqual(result);
+});
+it('downloadWelcomePng', async () => {
+  const expected = await fs.readFile(pathToWelcomePng, 'utf8');
+  const result = await fs.readFile(`${timeDir}/sitesedona-github-io_files/welcome.png`, 'utf8');
+  expect(expected).toEqual(result);
 });
--- src/index.js
+++ src/index.js
@@ -9,18 +9,21 @@ import _ from 'lodash/fp';
 const loadingFile = (urlLoc, pathForFile) => axios.get(urlLoc)
   .then(response => fs.writeFile(pathForFile, response.data, 'utf8'))
   .then(() => fs.readFile(pathForFile, 'utf-8'))
-  .then(data => data)
+  .then(data => data);
 
 const loadingResources = (urlLoc, resources, pathForResources) => {
   const urlAndResourceName = _.keys(resources).filter(n => resources[n].length !== 0)
-    .reduce((acc, e) => [...acc, ...resources[e].map(tail => {
-      return {
-        urlPath: new URL(tail, urlLoc).href,
-        resourceName: path.resolve(pathForResources, tail.split('/').splice(-1).join(''))
-      };
-    })], []);
+    .reduce((acc, e) => [...acc, ...resources[e]
+      .map(tail => (
+        {
+          urlPath: new URL(tail, urlLoc).href,
+          resourceName: path.resolve(pathForResources, tail.split('/').splice(-1).join('')),
+        }
+      ))], []);
+
   return fs.mkdir(pathForResources)
-    .then(() => urlAndResourceName.map(actualUrl => loadingFile(actualUrl.urlPath, actualUrl.resourceName)))
+    .then(() => urlAndResourceName
+      .map(actualUrl => loadingFile(actualUrl.urlPath, actualUrl.resourceName)));
 };
 
 const findResources = (html) => {
